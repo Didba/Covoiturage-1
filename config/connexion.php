5548077f7345e0b3d00b73128ba00ae73c3dc4c9
pikaboul <thomasboulongne@hotmail.com>
2014-11-18 19:53:08 +0100

    Connexion
    Changement du nom de la base dans le fichier de connexion PDO

--- config/connexion.php
+++ config/connexion.php
@@ -3,7 +3,7 @@
     // Connexion à la base de données: Si la base existe, aucune erreur n'est relevée, on indique dans la variable gloable "co" que tout est bon. Sinon, on se connecte sans préciser de base, et on l'indique par un message d'alerte.
     try
     {
-        $db = new PDO('mysql:host=localhost;dbname=progweb', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=covoiturage', 'root', '');
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $db->query("SET lc_time_names = 'fr_FR'");
         $db->exec("SET CHARACTER SET utf8");
