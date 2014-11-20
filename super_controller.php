0b04831ab7c4568ced01998e660ce701facec7a8
pikaboul <thomasboulongne@hotmail.com>
2014-11-20 11:58:57 +0100

    Amélioration du controleur
    Ajout de la fonction de deconnexion
    Modification des liens de includes (admin->view devenu view->admin)

--- super_controller.php
+++ super_controller.php
@@ -43,20 +43,20 @@ session_start();
 				/*-------------------------------------------------------*/
 				case 'trajets':
 
-					include_once 'admin/views/v_trajets.class.php';
+					include_once 'views/admin/v_trajets.class.php';
 					//On instancie alors la page correspondante
 					$page = new v_trajets("Liste des trajets");
 					$page->set_html($traj_manager->getList());
 					break;
 
 				/*-------------------------------------------------------*/
-				/*--- AFFICHAGE D'UN TRAJET PRÉCIS-- */
+				/*--- AFFICHAGE D'UN TRAJET PRÉCIS -- */
 				/*-------------------------------------------------------*/
 				case 'trajet':
 					//On vérifie si un ID a bien été fourni
 					if(isset($id))
 					{
-						include_once 'admin/views/v_trajet.class.php';
+						include_once 'views/admin/v_trajet.class.php';
 						$page = new v_trajet("Trajet " + $id);
 						$page->set_html($traj_manager->getTrajet($id));
 					}
@@ -72,7 +72,7 @@ session_start();
 				/*-------------------------------------------------------*/
 				case 'membres':
 
-					include_once 'admin/views/v_membres.class.php';
+					include_once 'views/admin/v_membres.class.php';
 					//On instancie alors la page correspondante
 					$page = new v_membres("Liste des membres");
 					$page->set_html($mb_manager->getList());
@@ -85,7 +85,7 @@ session_start();
 					//On vérifie si un ID a bien été fourni
 					if(isset($id))
 					{
-						include_once 'admin/views/v_membre.class.php';
+						include_once 'views/admin/v_membre.class.php';
 						$page = new v_membre("Membre " + $id);
 						$page->set_html($mb_manager->getMembre($id));
 					}
@@ -164,7 +164,7 @@ session_start();
 					}
 					else if(isset($id))
 					{
-						include_once 'admin/views/v_newMessage.class.php';
+						include_once 'views/admin/v_newMessage.class.php';
 						$page = new v_newMessage("Nouveau message");
 						$page->set_html($mb_manager->getMembre($id));
 					}
@@ -175,11 +175,19 @@ session_start();
 					}
 					break;
 
+				/*-------------------------------------------------------------------------------*/
+				/*--------------------------- DÉCONNEXION -------------------------*/
+				/*-------------------------------------------------------------------------------*/
+				case 'deconnexion':
+					session_destroy();
+					header('Location: index.php');
+					break;
+
 				case default:
-					include_once 'admin/views/v_trajets.class.php';
+					include_once 'views/admin/v_index.class.php';
 					//On instancie alors la page correspondante
-					$page = new v_trajets("Liste des trajets");
-					$page->set_html($traj_manager->getList());
+					$page = new v_index("Accueil administrateur");
+					$page->set_html();
 					break;
 
 			}//if(switch)
@@ -200,9 +208,10 @@ session_start();
 				/*-------------------------------------------------------------------------------*/
 
 				case 'profil':
-					include_once 'user/views/v_profile.class.php';
+					include_once 'views/user/v_profile.class.php';
 					//On instancie alors la page correspondante
 					$page = new v_trajets("Mon profil");
+					$page->set_user($mb_manager->get($_SESSION['id']));
 					$page->set_html($mb_manager->getMembre($_SESSION['id']));
 
 					break;
@@ -212,9 +221,10 @@ session_start();
 				/*-------------------------------------------------------*/
 				case 'trajets':
 
-					include_once 'user/views/v_trajets.class.php';
+					include_once 'views/user/v_trajets.class.php';
 					//On instancie alors la page correspondante
 					$page = new v_trajets("Liste des trajets");
+					$page->set_user($mb_manager->get($_SESSION['id']));
 					$page->set_html($traj_manager->getList());
 					break;
 
@@ -225,8 +235,9 @@ session_start();
 					//On vérifie si un ID a bien été fourni
 					if(isset($id))
 					{
-						include_once 'user/views/v_trajet.class.php';
+						include_once 'views/user/v_trajet.class.php';
 						$page = new v_trajet("Trajet " + $id);
+						$page->set_user($mb_manager->get($_SESSION['id']));
 						$page->set_html($traj_manager->getTrajet($id));
 					}
 					else
@@ -327,9 +338,20 @@ session_start();
 					header('Location: index.php?action=profil');
 					break;
 
-				default:
-					$_SESSION['msg'] = 'La page que vous demandez n\'existe pas';
-					header('Location: index.php')
+				/*-------------------------------------------------------------------------------*/
+				/*--------------------------- DÉCONNEXION -------------------------*/
+				/*-------------------------------------------------------------------------------*/
+				case 'deconnexion':
+					session_destroy();
+					header('Location: index.php');
+					break;
+
+				case default:
+					include_once 'views/user/v_index.class.php';
+					//On instancie alors la page correspondante
+					$page = new v_index("Accueil");
+					$page->set_user($mb_manager->get($_SESSION['id']));
+					$page->set_html();
 					break;
 			}//switch
 		}//if(user)
@@ -340,7 +362,7 @@ session_start();
 				/*------------------------ Formulaire de recherche ---------------------*/
 				/*-------------------------------------------------------------------------------*/
 				case 'recherche':
-					include_once 'admin/views/v_trajets.class.php';
+					include_once 'views/admin/v_trajets.class.php';
 					if (isset($_POST)) {
 						extract $_POST;
 						$datas = array("start" => $start,"finish" => $finish, "date" => $date, "time" => $time);
@@ -356,29 +378,17 @@ session_start();
 					}
 					break;
 
-				default:
-					# code...
+				case default:
+					include_once 'views/v_index.class.php';
+					//On instancie alors la page correspondante
+					$page = new v_index("Accueil");
+					$page->set_html();
 					break;
 			}
 		}//if(!user&&!admin)
 
 	}//if(isset($action))
 
-	/*-------------------------------------------------------------------------------*/
-	/*------------------ Si l'utilisateur n'est pas un admin -----------------*/
-	/*-------------------------------------------------------------------------------*/
-	else
-	{
-
-		else
-		{
-			header('Location: index.php');
-		}
-		if (condition)
-		{
-			# code...
-		}
-	}//if(action)
 
 
 
@@ -391,211 +401,209 @@ session_start();
 
 
 
+	// 		/*-------------------------------------------------------*/
+	// 		/*------- MODIFICATION D'UN produit -------*/
+	// 		/*-------------------------------------------------------*/
+	// 		case 'modifier':
+	// 			include_once 'views/v_modifier2.class.php';
+	// 			//Si le premier formulaire de modification a été soumis (transmission du nom de le produit à modifier)
+	// 			if (isset($_POST['modif_nom_x'])) {
+	// 				if(isset($_POST['id']))
+	// 				{
+	// 					$data = array("id"=>$_POST['id']);
+	// 				}
+	// 				else if(isset($_POST['nom']))
+	// 				{
+	// 					$data = array("nom"=>$_POST['nom']);
+	// 				}
+	// 				//On récupère le produit à modifier à l'aide de son nom, puis on affiche la vue correspondante avec les données de cet produit
+	// 				$produit = $manager->get($data);
+	// 				$page = new v_modifier("Modifier le produit");
+	// 				$page->set_html($manager->catManager->getList(),$produit);
+	// 			}
+	// 			//Sinon, on vérifie si le second formulaire avec les nouvelles données a été transmis. Si oui, on appelle la methode update du manager
+	// 			else
+	// 			{
+	// 				if (isset($_POST['modif_produit']))
+	// 				{
+	// 					$manager->update($_POST);
+	// 					$GLOBALS['msg'] = "Le produit " . $_POST['nom'] . " a bien été mis à jour";
+	// 				}
+	// 				//On affiche alors la vue de base de la page de modification
+	// 				$page = new v_modifier("Modifier un produit");
+	// 				$page->set_html($manager->catManager->getList());
+	// 			}
+	// 			break;
 
+	// 		/*-------------------------------------------------------*/
+	// 		/*---------------- DECONNEXION ----------------*/
+	// 		/*-------------------------------------------------------*/
+	// 		case 'deconnexion':
+	// 			include_once 'views/v_modifier2.class.php';
+	// 			session_destroy();
+	// 			header('Location: TD-04.php');
+	// 			break;
 
-			/*-------------------------------------------------------*/
-			/*------- MODIFICATION D'UN produit -------*/
-			/*-------------------------------------------------------*/
-			case 'modifier':
-				include_once 'views/v_modifier2.class.php';
-				//Si le premier formulaire de modification a été soumis (transmission du nom de le produit à modifier)
-				if (isset($_POST['modif_nom_x'])) {
-					if(isset($_POST['id']))
-					{
-						$data = array("id"=>$_POST['id']);
-					}
-					else if(isset($_POST['nom']))
-					{
-						$data = array("nom"=>$_POST['nom']);
-					}
-					//On récupère le produit à modifier à l'aide de son nom, puis on affiche la vue correspondante avec les données de cet produit
-					$produit = $manager->get($data);
-					$page = new v_modifier("Modifier le produit");
-					$page->set_html($manager->catManager->getList(),$produit);
-				}
-				//Sinon, on vérifie si le second formulaire avec les nouvelles données a été transmis. Si oui, on appelle la methode update du manager
-				else
-				{
-					if (isset($_POST['modif_produit']))
-					{
-						$manager->update($_POST);
-						$GLOBALS['msg'] = "Le produit " . $_POST['nom'] . " a bien été mis à jour";
-					}
-					//On affiche alors la vue de base de la page de modification
-					$page = new v_modifier("Modifier un produit");
-					$page->set_html($manager->catManager->getList());
-				}
-				break;
+	// 		default:
 
-			/*-------------------------------------------------------*/
-			/*---------------- DECONNEXION ----------------*/
-			/*-------------------------------------------------------*/
-			case 'deconnexion':
-				include_once 'views/v_modifier2.class.php';
-				session_destroy();
-				header('Location: TD-04.php');
-				break;
+	// 			// include_once 'views/v_produit.class.php';
+	// 			// $page = new v_produit("Liste des produits");
+	// 			// $page->set_html($manager->getList());
+	// 			break;
+	// 	}
+	// }
+	// else if(isset($_GET['action']))
+	// {
+	// 	switch ($_GET['action']) {
 
-			default:
+	// 		/*-------------------------------------------------------*/
+	// 		/*-------- RECHERCHE D'UN produit --------*/
+	// 		/*-------------------------------------------------------*/
 
-				// include_once 'views/v_produit.class.php';
-				// $page = new v_produit("Liste des produits");
-				// $page->set_html($manager->getList());
-				break;
-		}
-	}
-	else if(isset($_GET['action']))
-	{
-		switch ($_GET['action']) {
+	// 		case 'rechercher':
+	// 			include_once 'views/v_rechercher2.class.php';
 
-			/*-------------------------------------------------------*/
-			/*-------- RECHERCHE D'UN produit --------*/
-			/*-------------------------------------------------------*/
+	// 			if(isset($_GET['rech_av']))
+	// 			{
+	// 				unset($_GET['action']);
+	// 				unset($_GET['rech_av']);
+	// 				$page = new v_rechercher("Rechercher un produit");
+	// 				$page->set_html($manager->catManager->getList(),$manager->getList($_GET));
+	// 			}
+	// 			else
+	// 			{
+	// 				$page = new v_rechercher("Rechercher un produit");
+	// 				$page->set_html($manager->catManager->getList());
+	// 			}
+	// 			break;
 
-			case 'rechercher':
-				include_once 'views/v_rechercher2.class.php';
+	// 		/*-------------------------------------------------------*/
+	// 		/*----- CONNEXION ADMINISTRATEUR -----*/
+	// 		/*-------------------------------------------------------*/
 
-				if(isset($_GET['rech_av']))
-				{
-					unset($_GET['action']);
-					unset($_GET['rech_av']);
-					$page = new v_rechercher("Rechercher un produit");
-					$page->set_html($manager->catManager->getList(),$manager->getList($_GET));
-				}
-				else
-				{
-					$page = new v_rechercher("Rechercher un produit");
-					$page->set_html($manager->catManager->getList());
-				}
-				break;
+	// 		case 'admin':
+	// 			include_once 'views/v_connexion.class.php';
 
-			/*-------------------------------------------------------*/
-			/*----- CONNEXION ADMINISTRATEUR -----*/
-			/*-------------------------------------------------------*/
+	// 			if(isset($_POST['conn']))
+	// 			{
+	// 				extract($_POST);
+	// 				$mdp = sha1($mdp . SALT);
+	// 				$query = $db->prepare('SELECT * FROM utilisateurs WHERE login=:login AND mdp=:mdp');
+	// 				$query -> bindParam(':login', $login,PDO::PARAM_STR);
+	// 				$query -> bindParam(':mdp', $mdp,PDO::PARAM_STR);
+	// 				$query->execute() or die(print_r($query->errorInfo()));
 
-			case 'admin':
-				include_once 'views/v_connexion.class.php';
+	// 				$result = $query->fetchAll();
+	// 				if (count($result)==1) {
+	// 					$_SESSION['admin'] = $login;
+	// 					$GLOBALS['msg'] = 'Bonjour ' . $login;
+	// 				}
+	// 				else
+	// 				{
+	// 					$GLOBALS['msg'] = 'Login ou mot de passe erronné';
+	// 				}
+	// 				header('Location: TD-04.php');
+	// 			}
+	// 			else
+	// 			{
+	// 				$page = new v_connexion("Connexion des administrateurs");
+	// 				$page->set_html();
+	// 			}
+	// 			break;
 
-				if(isset($_POST['conn']))
-				{
-					extract($_POST);
-					$mdp = sha1($mdp . SALT);
-					$query = $db->prepare('SELECT * FROM utilisateurs WHERE login=:login AND mdp=:mdp');
-					$query -> bindParam(':login', $login,PDO::PARAM_STR);
-					$query -> bindParam(':mdp', $mdp,PDO::PARAM_STR);
-					$query->execute() or die(print_r($query->errorInfo()));
+	// 		/*-------------------------------------------------------*/
+	// 		/*--- AJOUT D'UN PRODUIT AU PANIER ---*/
+	// 		/*-------------------------------------------------------*/
 
-					$result = $query->fetchAll();
-					if (count($result)==1) {
-						$_SESSION['admin'] = $login;
-						$GLOBALS['msg'] = 'Bonjour ' . $login;
-					}
-					else
-					{
-						$GLOBALS['msg'] = 'Login ou mot de passe erronné';
-					}
-					header('Location: TD-04.php');
-				}
-				else
-				{
-					$page = new v_connexion("Connexion des administrateurs");
-					$page->set_html();
-				}
-				break;
+	// 		case 'aj_panier':
+	// 			include 'views/v_produit.class.php';
+	// 			$produit = array('id'=>$_POST['id'],'nom'=>$_POST['nom']);
+	// 			$push = false;
+	// 			foreach ($_SESSION['panier'] as $key => &$value) {
+	// 				if ($value['id']==$produit['id']) {
+	// 					$value['quantité']++;
+	// 					$push = true;
+	// 				}
+	// 			}
 
-			/*-------------------------------------------------------*/
-			/*--- AJOUT D'UN PRODUIT AU PANIER ---*/
-			/*-------------------------------------------------------*/
+	// 			if(!$push)
+	// 			{
+	// 				$produit = array('id'=>$produit['id'],'nom'=>$produit['nom'],'quantité'=>1);
+	// 				array_push($_SESSION['panier'],$produit);
+	// 			}
+	// 			$GLOBALS['msg'] = 'Le produit ' . $_POST['nom'] . ' a bien été ajouté au panier.';
+	// 			$page = new v_produit('Liste des produits');
+	// 			$page->set_html($manager->getList());
+	// 			break;
 
-			case 'aj_panier':
-				include 'views/v_produit.class.php';
-				$produit = array('id'=>$_POST['id'],'nom'=>$_POST['nom']);
-				$push = false;
-				foreach ($_SESSION['panier'] as $key => &$value) {
-					if ($value['id']==$produit['id']) {
-						$value['quantité']++;
-						$push = true;
-					}
-				}
+	// 		/*-------------------------------------------------------------------*/
+	// 		/*--- SUPPRESSION D'UN PRODUIT DU PANIER ---*/
+	// 		/*-------------------------------------------------------------------*/
 
-				if(!$push)
-				{
-					$produit = array('id'=>$produit['id'],'nom'=>$produit['nom'],'quantité'=>1);
-					array_push($_SESSION['panier'],$produit);
-				}
-				$GLOBALS['msg'] = 'Le produit ' . $_POST['nom'] . ' a bien été ajouté au panier.';
-				$page = new v_produit('Liste des produits');
-				$page->set_html($manager->getList());
-				break;
+	// 		case 'supp_panier':
+	// 			//var_dump($_SESSION['panier']);
+	// 			foreach ($_SESSION['panier'] as $key => &$value) {
+	// 				if ($value['id']==$_POST['id']) {
+	// 					if ($value['quantité']>1) {
+	// 						$value['quantité']--;
+	// 					}
+	// 					else
+	// 					{
+	// 						unset($_SESSION['panier'][$key]);
+	// 					}
+	// 				}
+	// 			}
+	// 			header('Location: TD-04.php?action=panier');
 
-			/*-------------------------------------------------------------------*/
-			/*--- SUPPRESSION D'UN PRODUIT DU PANIER ---*/
-			/*-------------------------------------------------------------------*/
+	// 			break;
 
-			case 'supp_panier':
-				//var_dump($_SESSION['panier']);
-				foreach ($_SESSION['panier'] as $key => &$value) {
-					if ($value['id']==$_POST['id']) {
-						if ($value['quantité']>1) {
-							$value['quantité']--;
-						}
-						else
-						{
-							unset($_SESSION['panier'][$key]);
-						}
-					}
-				}
-				header('Location: TD-04.php?action=panier');
+	// 		/*-------------------------------------------------------*/
+	// 		/*-------- VISUALISATION DU PANIER --------*/
+	// 		/*-------------------------------------------------------*/
 
-				break;
+	// 		case 'panier':
+	// 			include 'views/v_panier.class.php';
+	// 			if (empty($_SESSION['panier'])) {
+	// 				$GLOBALS['msg'] = 'Votre panier est vide.';
+	// 			}
+	// 			$panier = array();
+	// 			$id = 0;
+	// 			foreach ($_SESSION['panier'] as $key => $value) {
+	// 				$produit = $manager->get(array('id'=>$value['id']));
+	// 				$panier[$id] = array('produit'=>$produit,'quantité'=>$value['quantité']);
+	// 				$id++;
+	// 			}
+	// 			$page = new v_panier('Mon panier');
+	// 			$page->set_html($panier);
+	// 			break;
 
-			/*-------------------------------------------------------*/
-			/*-------- VISUALISATION DU PANIER --------*/
-			/*-------------------------------------------------------*/
+	// 		default:
+	// 			include 'views/Page.class.php';
+	// 			$GLOBALS['msg'] = 'Cette page n\'existe pas';
+	// 			$page = new Page('Boutique');
+	// 			break;
+	// 	}
+	// }
+	// //Sinon on affiche l'accueil, c'est à dire la liste des produits
+	// else
+	// {
+	// 		/*-------------------------------------------------------*/
+	// 		/*------------- LISTE DES produits ------------*/
+	// 		/*-------------------------------------------------------*/
 
-			case 'panier':
-				include 'views/v_panier.class.php';
-				if (empty($_SESSION['panier'])) {
-					$GLOBALS['msg'] = 'Votre panier est vide.';
-				}
-				$panier = array();
-				$id = 0;
-				foreach ($_SESSION['panier'] as $key => $value) {
-					$produit = $manager->get(array('id'=>$value['id']));
-					$panier[$id] = array('produit'=>$produit,'quantité'=>$value['quantité']);
-					$id++;
-				}
-				$page = new v_panier('Mon panier');
-				$page->set_html($panier);
-				break;
-
-			default:
-				include 'views/Page.class.php';
-				$GLOBALS['msg'] = 'Cette page n\'existe pas';
-				$page = new Page('Boutique');
-				break;
-		}
-	}
-	//Sinon on affiche l'accueil, c'est à dire la liste des produits
-	else
-	{
-			/*-------------------------------------------------------*/
-			/*------------- LISTE DES produits ------------*/
-			/*-------------------------------------------------------*/
-
-		if($GLOBALS['co'])
-		{
-			include_once 'views/v_produit.class.php';
-			$page = new v_produit("Liste des produits");
-			$page->set_html($manager->getList());
-		}
-		else
-		{
-			//Si le connexion n'a pas été validée, on envoie une page par défaut, dans laquelle le message d'alerte sera indiqué
-			include_once 'views/Page.class.php';
-			$page =  new Page("Boutique");
-		}
-	}
+	// 	if($GLOBALS['co'])
+	// 	{
+	// 		include_once 'views/v_produit.class.php';
+	// 		$page = new v_produit("Liste des produits");
+	// 		$page->set_html($manager->getList());
+	// 	}
+	// 	else
+	// 	{
+	// 		//Si le connexion n'a pas été validée, on envoie une page par défaut, dans laquelle le message d'alerte sera indiqué
+	// 		include_once 'views/Page.class.php';
+	// 		$page =  new Page("Boutique");
+	// 	}
+	// }
 
 	//On ajoute les feuilles de styles nécessaires à la page
 	$page->head->add_css("css/style.css");
