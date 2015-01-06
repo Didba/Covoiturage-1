fac76e2d7568f0e294a2e59bcd25fb6d9cce1316
pikaboul <thomasboulongne@hotmail.com>
2015-01-06 14:05:17 +0100

    Profil

--- views/v_profil.class.php
+++ views/v_profil.class.php
@@ -15,14 +15,14 @@
 			$html .= '<h4>Mon profil :</h4>';
 			$html .= '<ul class="results">';
 			$html .= '<img src="images/user.png" width="100" height="100" /><br />';
-			$html .= '<class="result"><br /><b>Nom :     </b>' . $adherent->nom() . '</class><br />';
-			$html .= '<br /><class="result"><b>Prenom :  </b>' . $adherent->prenom() . '</class><br />';
-			$html .= '<br /><class="result"><b>Sexe :    </b>' . $adherent->sexe() . '</class><br />';
-			$html .= '<br /><class="result"><b>Tel :     </b>'.  $adherent->telephone() . '</class><br />';
-			$html .= '<br /><class="result"><b>Date naiss : </b>' .   date('d/m/Y',strtotime($adherent->date_Naissance())) . '</class><br />';
-			$html .= '<br /><class="result"><b>Mail : </b>' .  $adherent->mail() . '</class><br />';
+			$html .= $adherent->prenom() ;
+			$html .= '' . $adherent->nom(). '<br />';
+			$html .= $adherent->sexe() . '<br />';
+			$html .= $adherent->telephone() . '<br />';
+			$html .=   date('d/m/Y',strtotime($adherent->date_Naissance())) . '<br />';
+			$html .=  $adherent->mail() . '<br />';
 
-			$html .= '<aside>';
+			$html .= '<div class="profil_nav">';
 			$html .= '<ul>';
 
 			if(isset($_SESSION['permis']))
@@ -34,7 +34,7 @@
 				<li><a href="super_controller.php?application=mes_trajets">Mes trajets</a></li>
 				<li><a href="super_controller.php?application=modif_profil">Modifier mon profil</a></li>
 			</ul>'	;
-			$html .= '</aside>';
+			$html .= '</div>';
 
 			//On retourne tout ce qu'on vient de créer en HTML dans l'attribut correspondant de la page
 			$this->html = $html;
